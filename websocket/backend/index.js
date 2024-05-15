0e6b3620cf1005a6481107831c5d94c861b0adec
Alban de Braquilanges <alban2bqs@gmail.com>
2024-05-15 10:49:34 +0200

    affichage de chaque userId à la connexion

--- websocket/backend/index.js
+++ websocket/backend/index.js
@@ -1,52 +1,53 @@
-import express from 'express';
-import http from 'http';
-import ip from 'ip';
-import { Server } from 'socket.io';
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import ip from "ip";
+import { Server } from "socket.io";
+import cors from "cors";
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-        }
-})
-app.use(cors())
-app.get('/', (req, res) => {
-    res.json('ip address: http://' + ip.address()+':'+PORT);    
+  cors: {
+    origin: "*",
+  },
+});
+app.use(cors());
+app.get("/", (req, res) => {
+  res.json("ip address: http://" + ip.address() + ":" + PORT);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.broadcast.emit('user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        socket.broadcast.emit('user disconnected');
-    });
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
-    
-    socket.on('room', (room, msg) => {
-        console.log('room: ' + room + ' message: ' + msg);
-        io.to(room).emit('message', msg);
-    });
+io.on("connection", (socket) => {
+  let userId =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+  console.log("a user connected with ID: ", userId);
+  socket.broadcast.emit("user connected", userId);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    socket.broadcast.emit("user disconnected");
+  });
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("message", msg);
+  });
 
-    socket.on('join', (room) => {
-        console.log('join room: ' + room);
-        socket.join(room);
-        io.to(room).emit('join', room);
-    });
-    socket.on('leave', (room) => {
-        console.log('leave room: ' + room);
-        socket.leave(room);
-        io.to(room).emit('leave', room);
-    });
-})
+  socket.on("room", (room, msg) => {
+    console.log("room: " + room + " message: " + msg);
+    io.to(room).emit("message", msg);
+  });
 
+  socket.on("join", (room) => {
+    console.log("join room: " + room);
+    socket.join(room);
+    io.to(room).emit("join", room);
+  });
+  socket.on("leave", (room) => {
+    console.log("leave room: " + room);
+    socket.leave(room);
+    io.to(room).emit("leave", room);
+  });
+});
 
 server.listen(PORT, () => {
-    console.log('Server ip : http://' +ip.address() +":" + PORT);
-})
-
+  console.log("Server ip : http://" + ip.address() + ":" + PORT);
+});
