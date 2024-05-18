13858d678fd4ebb4cdc3de03942901b63f26e07d
Alban de Braquilanges <alban2bqs@gmail.com>
2024-05-18 14:48:48 +0200

    cliquer sur les caes

--- websocket/front/bataille.js
+++ websocket/front/bataille.js
@@ -9,7 +9,7 @@ function generateGrid(player) {
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-      grid.innerHTML += `<div class="cell" id="${i}-${j}" style="background-color: white;"></div>`;
+      grid.innerHTML += `<div class="cell" id="${i}-${j}" style="background-color: white;" onclick="checkCell(this, ${player})"></div>`;
     }
   }
 
