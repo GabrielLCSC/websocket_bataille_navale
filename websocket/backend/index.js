02ece641ae60132325fbcbf7540df4206956ada1
Alban de Braquilanges <alban2bqs@gmail.com>
2024-05-19 14:15:57 +0200

    correction

--- websocket/backend/index.js
+++ websocket/backend/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const ip = require("ip");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
